11d5f1ea549d062b671a0c082c9f855ec79fa16d
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2023-01-02 11:08:40 +0700

    update

---
+++ riwayat.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Pencairan Dana | Litra</title>
+  <link href="./assets/css/bootstrap.min.css" rel="stylesheet">
+  <link href="./assets/font-awesome/css/font-awesome.css" rel="stylesheet">
+  <link href="style.css" rel="stylesheet">
+  <style>
+    body {
+      background: url('./assets/bg-1.jpeg') no-repeat center center fixed;
+      -webkit-background-size: cover;
+      -moz-background-size: cover;
+      -o-background-size: cover;
+      background-size: cover;
+    }
+  </style>
+</head>
+
+<body style="background-color: white;">
+  <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #000;">
+    <a class="navbar-brand" href="#">
+      <img src="./assets/logo.png" style="max-height: 40px;">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fa fa-money"></i> Rp 123.456</a>
+        </li>
+      </ul>
+    </div>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            <button class="btn btn-sm btn-primary">Home</button>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">News</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <div class="wrapper wrapper-content">
+    <h3 style="text-align: center; font-weight: bold; color: white">Riwayat</h3>
+    <div class="row" style="margin-top: 50px;">
+      <div class="col-md-6">
+        <div class="ibox card-2" style="height: 100px; text-align: center; font-weight: bold; padding: 20px 0px; ">
+          <h2>Riwayat Setor Sampah</h2>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="ibox card-2" style="height: 100px; text-align: center; font-weight: bold; padding: 20px 0px; ">
+          <h2>Riwayat Pencairan Dana</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="./assets/js/jquery-3.1.1.min.js"></script>
+  <script src="./assets/js/popper.min.js"></script>
+  <script src="./assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
